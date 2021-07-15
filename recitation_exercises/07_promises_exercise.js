a7315bde47382f2ef438cfd86998dc8cd1b0f2d5
James Bickerstaff <jjb169@pitt.edu>
2021-07-15 16:28:27 -0400

    recitation whatever

--- recitation_exercises/07_promises_exercise.js
+++ recitation_exercises/07_promises_exercise.js
@@ -10,7 +10,13 @@ You can verify this by printing something to the console
 after waiting for 2 seconds. Look into setTimeout function.
 */
 
-
+const taskOnePromise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("foo");
+		}, 2000);
+	});
+	
+taskOnePromise.then(() => {console.log("task 1");});	
 
 /*
 2) 
@@ -22,9 +28,16 @@ otherwise it rejects. For now, you can declare a variable
 that holds the number of players. Again you can print
 stuff to the console to verify your implementation works
 */
+let numplayers = 0;
+const playerPromise = new Promise((resolve, reject) => {
+	setInterval(() => { 
+		if (numplayers == 2){
+			resolve();
+		}
+	}, 200);
+	});
 
-
-
+playerPromise.then(() => {console.log("task 2");});
 /*
 3) 
 Now we will try to chain promises together. Notice
@@ -38,6 +51,26 @@ fetch the first file and 4 to fecth the second.
 Again you can print stuff to the console to verify
 your implementation works and use timeouts.
 */
+
+const downloadOnePromise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("foo");
+		}, 2000);
+	});
+
+downloadOnePromise.then(() => {
+		console.log("First File");
+	});
+	
+const downloadTwoPromise = downloadOnePromise.then(((resolve, reject) => {
+	setTimeout(() => {
+		resolve("foo");
+		}, 4000);
+	}));
+	
+downloadTwoPromise.then(() => {console.log("Second File");});
+
+
 
 
 
